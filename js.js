1487a4d7904290720c9d29fda1c5f0b4972a4530
Fahad Qayyum <fahadq@my.yorku.ca>
2019-05-17 16:08:33 -0400

    JS practice

--- js.js
+++ js.js
@@ -1,11 +1,7 @@
-var times = 0;
-var interval = setInterval(function(){
-	console.log("1st function!");
-	if (times == 5) {
-		clearInterval(interval);
-	times = times + 1
-	 setInterval(function(){
-	   console.log("2nd function!");
-	},500);
-	}
-},1000);
+Array.prototype.myfunction = function(name){
+	name();
+}; 
+function name(){
+	console.log("hello!");
+}
+myfunction("Fahad");
